3dd9d67c09eea0eeda6e4e9c10c61516d9234b95
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2019-03-15 01:40:55 +0100

    first commit :tada:

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'inside-the-scream-website-nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'scream website' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:400,400i,700' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=PT+Sans' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Righteous' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  mode: 'spa',
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    styleResources: {
+      scss: './assets/scss/main.scss'
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        '$': 'jquery',
+        '_': 'lodash'
+      })
+    ],
+    postcss: {
+      preset: {
+        autoprefixer: {
+          browsers: [
+            '> 5%'
+          ,
+            'ie > 10'
+          ]
+        }
+      }
+    },
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  router: {
+    base: '/website-nuxtjs/'
+  }
+}
+
